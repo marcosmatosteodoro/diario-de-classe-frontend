8bc5c2ac9fc751f3be6589d48931b9d997563141
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-01 13:49:02 -0300

    feat: wrap Layout component with ToastProvider for toast notifications

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Layout } from '@/components';
 import { QueryProvider } from '@/providers/QueryProvider';
+import { ToastProvider } from '@/providers/ToastProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +26,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>
-          <Layout>{children}</Layout>
+          <ToastProvider>
+            <Layout>{children}</Layout>
+          </ToastProvider>
         </QueryProvider>
       </body>
     </html>
