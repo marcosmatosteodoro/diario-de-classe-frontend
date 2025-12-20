e7b538b5ce77a1ddd0953a7ba2c5c0cdbb419237
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-12-20 13:36:15 -0300

    feat: refactor ContratoStep1 to improve aluno and professor options handling

--- src/app/(application)/contratos/formulario/step1/page.jsx
+++ src/app/(application)/contratos/formulario/step1/page.jsx
@@ -2,16 +2,30 @@
 
 import { useContratoForm } from '@/providers/ContratoFormProvider';
 import { useAlunos } from '@/hooks/alunos/useAlunos';
-import { SelectField } from '@/components';
+import { SelectField, FormGroup } from '@/components';
+import { useProfessores } from '@/hooks/professores/useProfessores';
 
 export default function ContratoStep1() {
   const { setAluno, formData, handleChange } = useContratoForm();
   const { alunos } = useAlunos();
-  const options =
+  const { professores } = useProfessores();
+  const makeLabel = ({ nome, sobrenome, email }) => {
+    return `${nome} ${sobrenome} <${email}>`;
+  };
+
+  // TODO não mostrar alunos com contratos ativos
+  const alunoOptions =
     alunos && alunos.length > 0
       ? alunos.map(aluno => ({
-          label: `${aluno.nome} ${aluno.sobrenome} <${aluno.email}>`,
+          label: makeLabel(aluno),
           value: aluno.id,
+        }))
+      : [];
+  const professorOptions =
+    professores && professores.length > 0
+      ? professores.map(professor => ({
+          label: makeLabel(professor),
+          value: professor.id,
         }))
       : [];
   const handleStep1Change = e => {
@@ -21,15 +35,27 @@ export default function ContratoStep1() {
       alunos.find(aluno => aluno.id.toString() === value.toString()) || null
     );
   };
+
   return (
-    <SelectField
-      required
-      htmlFor="alunoId"
-      label="Aluno"
-      placeholder="Selecione o aluno"
-      onChange={handleStep1Change}
-      value={formData.alunoId}
-      options={options}
-    />
+    <FormGroup col={2}>
+      <SelectField
+        required
+        htmlFor="alunoId"
+        label="Aluno"
+        placeholder="Selecione o aluno"
+        onChange={handleStep1Change}
+        value={formData.alunoId}
+        options={alunoOptions}
+      />
+      <SelectField
+        required
+        htmlFor="professorId"
+        label="Professor"
+        placeholder="Selecione o professor"
+        onChange={handleChange}
+        value={formData.professorId}
+        options={professorOptions}
+      />
+    </FormGroup>
   );
 }
