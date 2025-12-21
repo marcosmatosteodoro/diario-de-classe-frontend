5bd4a323fd2bee155bd7223c4dfdf341e2a67b1f
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-12-21 10:57:58 -0300

    feat: enhance Avatar component to accept custom className prop for styling

--- src/components/ui/Avatar/index.jsx
+++ src/components/ui/Avatar/index.jsx
@@ -1,15 +1,16 @@
 import { useAvatar } from './useAvatar';
 
-export const Avatar = ({ text }) => {
+export const Avatar = ({ text, className }) => {
   const { getTitleByText, getColorByText, getKeyByText } = useAvatar();
   const title = getTitleByText(text);
   const colorClass = getColorByText(text);
   const id = getKeyByText(text);
+  const finalClassName = className || 'w-14 h-14';
   return (
     <div
       key={id}
       id={id}
-      className={`w-14 h-14 rounded-full ${colorClass} flex items-center justify-center text-lg font-bold`}
+      className={` rounded-full ${colorClass} flex items-center justify-center text-lg font-bold ${finalClassName}`}
       data-testid="avatar-component"
     >
       {title}
